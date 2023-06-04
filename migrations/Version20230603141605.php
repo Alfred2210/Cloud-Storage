59bad38a8bd8684e2b2f9fd17b08a3a652cac549
Alfred2210 <alfredxavier.2210@gmail.com>
2023-06-04 22:39:03 +0200

    add Plans interface

---
+++ migrations/Version20230603141605.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230603141605 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('INSERT INTO plan (nom, capacite, prix) VALUES ("Basic", 20, 20)');
+        $this->addSql('INSERT INTO plan (nom, capacite, prix) VALUES ("Standard", 50, 50)');
+        $this->addSql('INSERT INTO plan (nom, capacite, prix) VALUES ("Premium", 100, 100)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE plan');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
--- src/Controller/PlanController.php
+++ src/Controller/PlanController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PlanRepository;
 
 class PlanController extends AbstractController
 {
+private $planRepository;
+
+
+    public function __construct(PlanRepository $planRepository)
+    {
+        $this->planRepository = $planRepository;
+    }
+
+
     #[Route('/plan', name: 'app_plan')]
     public function index(): Response
     {
+
+        $plans = $this->planRepository->findAll();
+
         return $this->render('plan/index.html.twig', [
-            'controller_name' => 'PlanController',
+            'plans' => $plans,
         ]);
     }
 }
