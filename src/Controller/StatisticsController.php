60fb847e4c4a03e26ce62f582240b3bc9d36d81e
Alfred2210 <alfredxavier.2210@gmail.com>
2023-08-07 11:40:13 +0200

    add statistics

--- src/Controller/StatisticsController.php
+++ src/Controller/StatisticsController.php
@@ -29,15 +29,42 @@ class StatisticsController extends AbstractController
         $totalFiles = $entityManager->getRepository(File::class)->count([]);
 
         // Le nombre de fichiers uploadés aujourd'hui
-        $today = new DateTime('today');
-        $filesToday = $entityManager->getRepository(File::class)->count(['date' => $today]);
+        $today = new \DateTime('today');
+        $filesToday = $entityManager->getRepository(File::class)
+            ->createQueryBuilder('f')
+            ->select('COUNT(f)')
+            ->where('f.date >= :today')
+            ->setParameter('today', $today)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+
+
 
         // La répartition du nombre de fichiers par client
         //$clientsFiles = $entityManager->getRepository(File::class)->countFilesByClient();
 
+        $fileTypes = [
+            'application/pdf' => 'filePdf',
+            'image/jpeg' => 'fileJpg',
+            'image/png' => 'filePng',
+            'application/VND.OPENXMLFORMATS-OFFICEDOCUMENT.WORDPROCESSINGML.DOCUMENT' => 'fileDoc',
+            'text/plain' => 'fileTxt'
+        ];
+
+        foreach ($fileTypes as $type => $FileType) {
+            $$FileType = $entityManager->getRepository(File::class)->count(['type' => $type]);
+        }
+
         return $this->render('statistics/index.html.twig', [
             'totalFiles' => $totalFiles,
             'filesToday' => $filesToday,
+            'filePdf' => $filePdf,
+            'fileJpg' => $fileJpg,
+            'filePng' => $filePng,
+            'fileDoc' => $fileDoc,
+            'fileTxt' => $fileTxt
+
             //'clientsFiles' => $clientsFiles,
         ]);
     }
